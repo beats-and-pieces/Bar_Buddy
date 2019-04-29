b291465e4b0eb74867d8066a8fdbfbb343a18e7a
Anton <Anton@antons-macbook-pro.local>
2019-04-29 14:02:59 +0300

    Initial Commit

---
+++ Bar Buddy/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Bar Buddy
+//
+//  Created by Anton Kuznetsov on 29/04/2019.
+//  Copyright © 2019 Anton Kuznetsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Bar Buddy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bar Buddy
+//
+//  Created by Anton Kuznetsov on 29/04/2019.
+//  Copyright © 2019 Anton Kuznetsov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
