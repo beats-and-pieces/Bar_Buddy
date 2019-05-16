0beeca3da2661d9d297fb5e122f4e16dd495a1b0
Anton <Anton@antons-macbook-pro.local>
2019-05-16 20:23:50 +0300

    pre OCMock

---
+++ Bar BuddyTests/TestMapViewController.m
@@ -0,0 +1,39 @@
+//
+//  TestMapViewController.m
+//  Bar BuddyTests
+//
+//  Created by Anton Kuznetsov on 16/05/2019.
+//  Copyright © 2019 Anton Kuznetsov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "MapViewController.h"
+
+@interface TestMapViewController : XCTestCase
+
+@end
+
+@implementation TestMapViewController
+
+
+- (void)testTabBarItemTitle
+{
+    
+    MapViewController *mvp = [MapViewController alloc] initWithDataManager:<#(DataManager *)#>
+    - (NSString *)getTabBarItemTitle;
+    
+    
+    
+    
+    XCTAssertTrue([[assembly getRootViewController] isKindOfClass:[UIViewController class]], @"Cannot find ViewController instance");
+    // no teardown needed
+    
+    
+}
+// This is an example of a functional test case.
+// Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+
+
+@end
